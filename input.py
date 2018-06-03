dfba79e032d20ecd4d33aecca36400bf828c1e9e
pythondevelope <39884808+pythondevelope@users.noreply.github.com>
2018-06-03 20:21:34 +0200

    Create input.py

---
+++ input.py
@@ -0,0 +1,14 @@
+x = input("Your Name : ")
+print("Welcome",x)
+
+x = input("Years : ")
+print("Your Years Is",x)
+
+x = input("Do you like program? 0-10 :")
+print("Thanks for rating")
+print("Your Rating is",x)
+
+x = input("End? : ")
+print("Your answer is",x)
+print("© 2018")
+print("Thanks For Using my python app.")
